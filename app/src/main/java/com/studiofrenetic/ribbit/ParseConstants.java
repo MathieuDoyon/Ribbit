0165281e26e1173823c97a8d41aa7d2cebfbdb91
Mathieu Doyon <mathieu.doyon@studiofrenetic.com>
2014-06-20 16:55:31 -0400

    Add friends checkmarks
    Signed-off-by: Mathieu Doyon <mathieu.doyon@studiofrenetic.com>

---
+++ app/src/main/java/com/studiofrenetic/ribbit/ParseConstants.java
@@ -0,0 +1,12 @@
+package com.studiofrenetic.ribbit;
+
+/**
+ * Created by mathieu on 2014-06-20.
+ */
+public class ParseConstants {
+    // Class name
+
+    // Fields names
+    public static final String KEY_USERNAME = "username";
+    public static final String KEY_FRIENDS_RELATION = "FriendsRelation";
+}
