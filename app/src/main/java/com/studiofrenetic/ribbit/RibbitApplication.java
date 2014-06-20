88c1549d388d95f40e7b621441e0ceffad84cedf
Mathieu Doyon <mathieu.doyon@studiofrenetic.com>
2014-06-20 09:27:30 -0400

    Add Login and Logout
    Signed-off-by: Mathieu Doyon <mathieu.doyon@studiofrenetic.com>

---
+++ app/src/main/java/com/studiofrenetic/ribbit/RibbitApplication.java
@@ -0,0 +1,22 @@
+package com.studiofrenetic.ribbit;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseInstallation;
+import com.parse.PushService;
+
+/**
+ * Created by mathieu on 2014-06-19.
+ */
+public class RibbitApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(this, "5pA7eoWbGXpkSH55vvDVGbURj3MHJfa3IUl3nlkK", "yGYJxTRJSLtyLL9y3zzpTDmUR4LogalJiSyd8FBd");
+
+        PushService.setDefaultPushCallback(this, MainActivity.class);
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+    }
+}
