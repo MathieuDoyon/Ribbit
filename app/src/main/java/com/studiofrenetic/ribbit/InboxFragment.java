4a907c7b7d4a8925db1475480f1eb030c5f17564
Mathieu Doyon <mathieu.doyon@studiofrenetic.com>
2014-06-20 10:36:27 -0400

    Add friends and Inbox fragments
    Signed-off-by: Mathieu Doyon <mathieu.doyon@studiofrenetic.com>

---
+++ app/src/main/java/com/studiofrenetic/ribbit/InboxFragment.java
@@ -0,0 +1,22 @@
+package com.studiofrenetic.ribbit;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by mathieu on 2014-06-20.
+ */
+public class InboxFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_inbox, container, false);
+
+
+        return rootView;
+    }
+}
