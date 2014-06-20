3119b47736428db7c1aac201f96cb13e6b7a0944
Mathieu Doyon <mathieu.doyon@studiofrenetic.com>
2014-06-20 17:19:09 -0400

    Add friends Segment
    Signed-off-by: Mathieu Doyon <mathieu.doyon@studiofrenetic.com>

--- app/src/main/java/com/studiofrenetic/ribbit/FriendsFragment.java
+++ app/src/main/java/com/studiofrenetic/ribbit/FriendsFragment.java
@@ -1,15 +1,33 @@
 package com.studiofrenetic.ribbit;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseRelation;
+import com.parse.ParseUser;
+
+import java.util.List;
 
 /**
  * Created by mathieu on 2014-06-20.
  */
 public class FriendsFragment extends ListFragment {
+
+    public static final String TAG = ListFragment.class.getSimpleName();
+
+    protected List<ParseUser> mFriends;
+    protected ParseRelation<ParseUser> mFriendsRelation;
+    protected ParseUser mCurrentUser;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -18,5 +36,43 @@ public class FriendsFragment extends ListFragment {
 
 
         return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        mCurrentUser = ParseUser.getCurrentUser();
+        mFriendsRelation = mCurrentUser.getRelation(ParseConstants.KEY_FRIENDS_RELATION);
+
+        mFriendsRelation.getQuery().findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> friends, ParseException e) {
+                if (e == null) {
+                    mFriends = friends;
+                    String[] usernames = new String[mFriends.size()];
+                    int i = 0;
+                    for (ParseUser friend : mFriends) {
+                        usernames[i] = friend.getUsername();
+                        i++;
+                    }
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                            getListView().getContext(),
+                            android.R.layout.simple_list_item_1,
+                            usernames);
+                    setListAdapter(adapter);
+                }
+                else {
+                    Log.e(TAG, e.getMessage());
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getListView().getContext());
+                    builder.setMessage(e.getMessage())
+                            .setTitle(getString(R.string.error_title))
+                            .setPositiveButton(android.R.string.ok, null);
+
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+            }
+        });
     }
 }
